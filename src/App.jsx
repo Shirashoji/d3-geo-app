aa8debd55762f87e61321262622e7affca1a0fb4
Shirashoji <46389417+Shirashoji@users.noreply.github.com>
2024-06-16 11:31:26 +0900

    componentsに分離

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,6 @@
-import * as d3 from "d3";
+import Map from "./components/vis/Map";
 import mapData from "./assets/Chofu-Polygons.geo.json";
 
 export default function App() {
-  const data = mapData;
-
-  const width = 800;
-  const height = 600;
-
-  const projection = d3.geoMercator().fitSize([width, height], data);
-  const path = d3.geoPath().projection(projection);
-
-  return (
-    <svg width={width} height={height}>
-      {data.features.map((d, i) => (
-        <path
-          key={i}
-          d={path(d)}
-          fill="none"
-          stroke="black"
-          strokeWidth={0.5}
-        />
-      ))}
-    </svg>
-  );
+  return <Map mapData={mapData} />;
 }
